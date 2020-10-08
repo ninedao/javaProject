e5a44687ec41ff229423bec883686d9964efa7cb
ninedao <lzc_liuzhichen@163.com>
2020-10-08 20:15:51 +0800

    normal submit

---
+++ src/com/lzc/thinkingInJava/arrays/E01_ArrayInitialization.java
@@ -0,0 +1,17 @@
+package com.lzc.thinkingInJava.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class E01_ArrayInitialization {
+    public static void print(BerylliumSphere[] a) {
+        for(BerylliumSphere b : a) {
+            System.out.println(b);
+        }
+    }
+    public static void main(String[] args) {
+        print(new BerylliumSphere[]{new BerylliumSphere(), new BerylliumSphere(), new BerylliumSphere()});
+        List[] s = new List[10];
+        //print({new BerylliumSphere(), new BerylliumSphere(), new BerylliumSphere()});
+    }
+}
