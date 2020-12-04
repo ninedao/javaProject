6c85f0f1d562fadeba8fd482c643270e8bc86055
lzc <lzc_liuzhichen@163.com>
2020-12-04 10:42:16 +0800

    normal commit

---
+++ src/com/lzc/thinkingInJava/containers/Test.java
@@ -0,0 +1,9 @@
+package com.lzc.thinkingInJava.containers;
+
+public abstract class Test<C> {
+    String name;
+    public Test(String name){
+        this.name = name;
+    }
+    abstract int test(C container, TestParam tp);
+}
